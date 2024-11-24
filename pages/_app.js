a75e3ebfca380fa1f61fb284b2cc4be77a0a7737
faizshaikh07050 <faizsshaikh@gmail.com>
2024-11-24 21:37:55 +0530

    Started the website main structure

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import MasterLayout from '../src/components/layouts/master';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../src/styles/style.scss";
+// import { Provider } from 'react-redux';
+// import { store, persistor } from "../src/redux/store";
+// import { PersistGate } from 'redux-persist/lib/integration/react';
+
+
+
+function MyApp({ Component, pageProps }) {
+    return (
+        // <Provider store={store}>
+        // <PersistGate loading={null} persistor={persistor}>
+        <MasterLayout>
+            <Component {...pageProps} />
+        </MasterLayout>
+        // </PersistGate>
+        // </Provider>
+    )
+}
+
+export default MyApp
---
+++ pages/_document.js
@@ -0,0 +1,72 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta charset="utf-8" />
+                    {/* <link rel="icon" href="/images/favicon.png" /> */}
+
+                    <meta property="og:title" content="MEF" />
+                    <meta property="og:description" content="MEF" />
+                    {/* <meta property="og:image" content="/images/displayimagenew.png" /> */}
+                    {/* <meta property="og:image:width" content="800" /> */}
+                    {/* <meta property="og:image:height" content="600" /> */}
+                    {/* <meta property="og:image:alt" content="MEF" /> */}
+
+                    <link rel="shortcut icon" href="/favicon.ico" />
+
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Secular+One&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital@0;1&display=swap" rel="stylesheet"></link>
+
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap"
+                        rel="stylesheet"
+                    />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+                        rel="stylesheet"
+                    ></link>
+                    <link
+                        rel="stylesheet"
+                        href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+                        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+                        crossorigin="anonymous"
+                    />
+                    <link
+                        rel="stylesheet"
+                        type="text/css"
+                        charset="UTF-8"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                    />
+                    <link
+                        rel="stylesheet"
+                        type="text/css"
+                        href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                    />
+
+                    {/* Second Microsoft Clarity script */}
+
+
+                    {/* //  <!-- Google tag (gtag.js) --> */}
+
+                </Head>
+                <body>
+                    {/* Add Google Tag Manager (noscript) */}
+
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
